33c3489effcfd8db608efdd03e1c6bb3bbffaaf2
muhammadganiev <mukhammad.ganiev@mail.ru>
2024-01-01 17:12:39 +0400

    delete

--- IrcErrorException.cpp
+++
@@ -1,7 +0,0 @@
-#include "IrcErrorException.hpp"
-
-IrcErrorException::IrcErrorException(const char *what_arg) : what_arg(what_arg) {}
-
-const char * IrcErrorException::what ( ) const throw () {
-	return what_arg;
-}
