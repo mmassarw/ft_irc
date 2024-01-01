792d2f415c769722ea97c5ef11eb416536312c69
muhammadganiev <mukhammad.ganiev@mail.ru>
2024-01-01 20:49:44 +0400

    names, nick, notice, oper, part added

---
+++ src/cmd/notice.cpp
@@ -0,0 +1,59 @@
+#include "IrcServer.hpp"
+#include "libft.hpp"
+
+//allowing a user to send notices to other users, channels ...
+int IrcServer::notice(User &u, const IRC::Message &m)
+{
+	if (!u.isRegistered() || m.params().size() < 2)
+		return (1);
+
+	Params				targets(m.params()[0].split());
+	const IRC::Param	&text(m.params()[1]);
+
+	for (Params::const_iterator target = targets.begin(); target != targets.end(); ++target)
+	{
+		if (target->isMask())
+		{
+			if (!u.umode().isSet(UserMode::OPERATOR))
+				continue ;
+			const std::string mask = target->mask().substr(1);
+			if ((*target)[0] == '$')
+			{
+				if (ft::match(mask, config.servername))
+					for (Network::UserMap::const_iterator i = network.users().begin(); i != network.users().end(); ++i)
+					{
+						User *v(i->second);
+						if (!v->hopcount() && v->isRegistered() && v->socket() != u.socket())
+							v->writeLine((IRC::MessageBuilder(u.prefix(), m.command()) << *target << text).str());
+					}
+			}
+			else if ((*target)[0] == '#')
+			{
+				size_t dot;
+				std::string toplevel;
+
+				if ((dot = target->find_last_of('.')) == std::string::npos)
+					continue ;
+				toplevel = target->substr(dot);
+				if (toplevel.find('*') == std::string::npos && toplevel.find('?') == std::string::npos)
+					for (Network::UserMap::const_iterator it = network.users().begin(); it != network.users().end(); ++it)
+						if (ft::match(mask, it->second->socket()->host()))
+							it->second->writeLine((IRC::MessageBuilder(it->second->prefix(), m.command()) << *target << text).str());
+			}
+		}
+		if (target->isNickname())
+		{
+			User *receiver = network.getByNickname(*target);
+			if (receiver)
+				receiver->writeLine((IRC::MessageBuilder(u.prefix(), m.command()) << *target << text).str());
+		}
+		else if (target->isChannel())
+		{
+			Channel *chan = network.getByChannelname(*target);
+			if (chan && chan->canSendToChannel(&u))
+				chan->send((IRC::MessageBuilder(u.prefix(), m.command()) << chan->name() << text).str(), &u);
+		}
+	}
+	u.idle() = ::time(NULL);
+	return (0);
+}
---
+++ src/cmd/oper.cpp
@@ -0,0 +1,19 @@
+#include "IrcServer.hpp"
+#include "libft.hpp"
+
+//give operator privileges to a user.
+int IrcServer::oper(User &u, const IRC::Message &m)
+{
+	if (!u.isRegistered())
+		return (writeNum(u, IRC::Error::notregistered()));
+	if (m.params().size() < 2)
+		return (writeNum(u, IRC::Error::needmoreparams(m.command())));
+	IrcServerConfig::OperMap::const_iterator io = config.opers.find(m.params()[0]);
+	if (io == config.opers.end() || io->second.pass != m.params()[1])
+		return (writeNum(u, IRC::Error::passwdmissmatch()));
+	if (!ft::match(io->second.host, u.prefix()))
+		return (writeNum(u, IRC::Error::nooperhost()));
+	writeNum(u, IRC::Reply::youreoper());
+	u.setUmode(u.umode().flags() | UserMode::OPERATOR);
+	return (0);
+}
