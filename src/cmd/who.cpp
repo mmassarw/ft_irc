b80f1d6b0b2279d52bd1d9e37f4622ae7d289f17
muhammadganiev <mukhammad.ganiev@mail.ru>
2024-01-02 16:53:50 +0400

    commands added: stats, time, topic, user, userhost, version, who, whois,whowas

---
+++ src/cmd/who.cpp
@@ -0,0 +1,143 @@
+#include "IrcServer.hpp"
+#include "libft.hpp"
+
+// basic information about users and channels based on a specified mask.  
+// o = parameter can be used to filter for operators.
+
+int IrcServer::who(User &u, const IRC::Message &m)
+{
+	if (!u.isRegistered())
+		return (writeNum(u, IRC::Error::notregistered()));
+
+	Params pMsg = m.params();
+
+	bool noParams = false;
+	std::string mask;
+	if (!pMsg.size())
+	{
+		noParams = true;
+		mask = "";
+	}
+	else
+		mask = pMsg[0];
+
+	bool ope = false;
+	if (pMsg.size() > 1)
+		if (pMsg[1] == "o")
+			ope = true;
+
+	const Network::ChannelMap &channels = network.channels();
+	Network::ChannelMap::const_iterator chanIter = channels.begin();
+	while (chanIter != channels.end() && !noParams)
+	{
+		Channel *c = chanIter->second;
+		ChannelMode cmode = c->mode();
+		if ((cmode.isSet(ChannelMode::PRIVATE)
+		|| cmode.isSet(ChannelMode::SECRET)) && !c->findMember(&u))
+		{
+			++chanIter;
+			continue;
+		}
+
+		if (ft::match(mask, c->name()))
+		{
+			const Channel::MemberMap &members = c->members();
+			Channel::MemberMap::const_iterator memberIter = members.begin();
+			while (memberIter != members.end())
+			{
+				User *us = memberIter->first;
+				MemberMode *mmode = c->findMember(us);
+				UserMode umode = us->umode();
+
+				if ((ope && (!umode.isSet(UserMode::OPERATOR)
+				&& !mmode->isSet(UserMode::OPERATOR)))
+				|| umode.isSet(UserMode::INVISIBLE))
+				{
+					++memberIter;
+					continue;
+				}
+
+				// ---------------------------------------------- //
+
+
+				std::string msg = "";
+
+				msg += c->name() + " " + us->username();
+				msg += " " + us->socket()->host();
+				msg += " " + config.servername + " " + us->nickname();
+
+
+				if (!umode.isSet(UserMode::AWAY))
+					msg += " H";
+				else
+					msg += " G";
+				if (umode.isSet(UserMode::OPERATOR))
+					msg += "*";
+				if (mmode->isSet(MemberMode::OPERATOR))
+					msg += "@";
+
+
+
+				msg += " :0 " + us->realname();
+
+				// ---------------------------------------------- //
+
+				writeNum(u, IRC::Reply::whoreply(msg));
+
+				++memberIter;
+
+			}
+			writeNum(u, IRC::Reply::endofwho(mask));
+			return 0;
+		}
+		++chanIter;
+	}
+
+	const Network::UserMap &users = network.users();
+	Network::UserMap::const_iterator userIter = users.begin();
+	while (userIter != users.end())
+	{
+		User *us = userIter->second;
+		if (ft::match(mask, us->nickname())
+		|| ft::match(mask, us->realname())
+		|| ft::match(mask, us->username())
+		|| ft::match(mask, config.servername)
+		|| noParams)
+		{
+			// ---------------------------------------------- //
+
+			UserMode umode = us->umode();
+
+			if ((ope && !umode.isSet(UserMode::OPERATOR))
+			|| umode.isSet(UserMode::INVISIBLE))
+			{
+				++userIter;
+				continue;
+			}
+
+
+			std::string msg;
+
+			msg += "* " + us->username();
+			msg += " " + us->socket()->host();
+			msg += " " + config.servername + " " + us->nickname();
+
+			if (!umode.isSet(UserMode::AWAY))
+				msg += " H";
+			else
+				msg += " G";
+			if (umode.isSet(UserMode::OPERATOR))
+				msg += "*";
+
+			msg += " :0 " + us->realname();
+
+			// ---------------------------------------------- //
+
+			writeNum(u, IRC::Reply::whoreply(msg));
+		}
+		++userIter;
+	}
+
+	writeNum(u, IRC::Reply::endofwho(mask));
+	return (0);
+}
---
+++ src/cmd/whowas.cpp
@@ -0,0 +1,32 @@
+#include "IrcServer.hpp"
+#include "libft.hpp"
+
+//historical information about users who were previously connected to the IRC network but are not currently online.
+
+int IrcServer::whowas(User &sender, const IRC::Message &m)
+{
+	if (!sender.isRegistered())
+		return (writeNum(sender, IRC::Error::notregistered()));
+	if (m.params().size() == 0)
+		return (writeNum(sender, IRC::Error::nonicknamegiven()));
+	Params targets = m.params()[0].split(5);
+	int count = 0;
+	if (m.params().size() > 1)
+		count = atoi(m.params()[1].c_str());
+	if (m.params().size() > 2)
+		if (!ft::match(m.params()[2].mask(), config.servername))
+			return writeNum(sender, IRC::Error::nosuchserver(m.params()[2]));
+	for (Params::const_iterator target = targets.begin(); target != targets.end(); ++target)
+	{
+		Network::InfoVec h = network.getNickHistory(*target, count);
+		if (!h.size())
+			writeNum(sender, IRC::Error::wasnosuchnick(*target));
+		else
+			for (size_t i = 0; i < h.size(); ++i)
+			{
+				writeNum(sender, IRC::Reply::whowasuser(*target, h[i].username, h[i].host, h[i].realname));
+				writeNum(sender, IRC::Reply::whoisserver(*target, config.servername, config.shortinfo));
+			}
+	}
+	return writeNum(sender, IRC::Reply::endofwhowas(sender.nickname()));
+}
